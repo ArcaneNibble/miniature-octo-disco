befd5c809883df7e7d54d9df0c50b70160ab19ff
Anonymous <>
2022-08-30 23:48:32 +0100

    Fix typo

--- mqtt_down.py
+++ mqtt_down.py
@@ -136,17 +136,17 @@ async def doOutput(message):
 				reply_cb_thing.clear()
 				await ble_client.start_notify(response_characteristic, reply_cb_thing.reply_cb)
 				await ble_client.write_gatt_char(command_characteristic, pack_frame(20, bytes([])), False)
-				asyncio.wait_for(reply_cb_thing, timeout=5)
+				await asyncio.wait_for(reply_cb_thing, timeout=5)
 				reply_cb_thing.clear()
 				voice = message["voice"] if "voice" in message else 0
 				volume = message["vol"] if "vol" in message else 0
-				voice = message["vibration"] if "vibration" in message else 0
-				voice = message["shock"] if "shock" in message else 50
-				await ble_client.write_gatt_char(command_characteristic, create_config_pkt(), False)
-				asyncio.wait_for(reply_cb_thing, timeout=5)
+				vibration = message["vibration"] if "vibration" in message else 0
+				shock = message["shock"] if "shock" in message else 50
+				await ble_client.write_gatt_char(command_characteristic, create_config_pkt(voice, volume, vibration, shock), False)
+				await asyncio.wait_for(reply_cb_thing, timeout=5)
 				reply_cb_thing.clear()
 				await ble_client.write_gatt_char(command_characteristic, pack_frame(0x2A, bytes([])), False)
-				asyncio.wait_for(reply_cb_thing, timeout=5)
+				await asyncio.wait_for(reply_cb_thing, timeout=5)
 				reply_cb_thing.clear()
 			elif (message["mode"] == "shock"):
 				reply_cb_thing.clear()
